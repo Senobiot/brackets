3867e6b345dedc3eb70098df81b6d41c5b9ca857
Aliaksandr Kulik <inter_net@tut.by>
2020-10-06 19:56:45 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+		let reg = bracketsConfig.join("|").replace(/,/g, ""), //for numbers
+			regexNum = new RegExp(reg,"g"),
+  			regex = /\[\]|\{\}|\(\)|\<\>|\|\|/g;
+  		if (bracketsConfig.join("").match(/\d/g) != null) regex = regexNum;
+        while (str.match(regex) != null) {str = str.replace(regex, "")}
+        return str.length == 0
 }
